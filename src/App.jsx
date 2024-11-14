d43238fe18078fcefbb32a30c8dc73228adcd8c9
Batchuluun0605 <batchuluun@gmail.com>
2024-11-14 21:42:49 +0800

    admin dashboard

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./ui/Main";
+import Content from "./ui/Content";
+import Profile from "./components/Profile/Profile";
+import Stats from "./components/Stats/Stats";
+import Team from "./components/Team/Team";
+import Event from "./components/Event/Event";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <div className={` ${darkMode && "dark"} font-quickSand `}>
+      <Header
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+        toggleSidebar={toggleSidebar}
+      />
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+      <Main>
+        <Content>
+          <Stats darkMode={darkMode} />
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <Team />
+            <Event />
+          </div>
+        </Content>
+        <Profile darkMode={darkMode} />
+      </Main>
+    </div>
+  );
+}
+
+export default App;
